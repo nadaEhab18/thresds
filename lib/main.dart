32ce06677c44922302f92e27739ada6c9f470e5c
nadaEhab18 <nehab7947@gmail.com>
2023-08-27 17:04:23 +0300

    get changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:thresds/firebase_options.dart';
 import 'package:thresds/screens/post_screen.dart';
-import 'package:thresds/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main()  async{
   WidgetsFlutterBinding.ensureInitialized();
